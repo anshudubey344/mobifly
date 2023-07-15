9c4232a1920a63be9610e507b7f407306f1bcdc3
Ubuntu <ubuntu@ip-172-31-84-68.ec2.internal>
2023-07-15 11:30:35 +0000

    tf automated file create

---
+++ day1/terraform-local/local.tf
@@ -0,0 +1,4 @@
+resource "local_file" "devops" {
+	filename = "/home/ubuntu/terraform/day1/terraform-local/devops_automated.txt"
+	content  = "I want to become a DevOps Engineer who knows Terraform"
+}
