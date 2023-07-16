89d947fcd1ffd7f34c8229f9589230baba77ef6e
Ubuntu <ubuntu@ip-172-31-84-68.ec2.internal>
2023-07-16 14:15:54 +0000

    nginx file

--- day1/terraform-docker/main.tf
+++ day1/terraform-docker/main.tf
@@ -1,27 +1,22 @@
 terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.23.0"
-    }
-  }
+	required_providers {
+	docker = {
+	source = "kreuzwerker/docker"
+	version = "3.0.2"
 }
-
+}
+}
 provider "docker" {}
-
 resource "docker_image" "nginx" {
-
-  name         = "nginx:latest"
-  keep_locally = false
+ name = "nginx"
+ keep_locally = false
 }
 
 resource "docker_container" "nginx" {
-
-  image = docker_image.nginx.latest
-  name  = "nginx-tf"
-  ports {
-
-    internal = 80
-    external = 80
-  }
+ image = docker_image.nginx.image_id
+ name = "nginx-tf"
+ ports {
+	internal = 80
+	external = 80
+}
 }
